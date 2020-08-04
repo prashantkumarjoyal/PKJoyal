2e2bacd8a6b8aae91595f814fd25ec11ab4de590
Prashant Kumar Joyal <40768163+prashantkumarjoyal@users.noreply.github.com>
2020-08-04 17:55:28 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,69 +1,29 @@
-fff* {
-  font-family: 'Raleway', sans-serif;
+function myFunction() {
+  var x = document.getElementById("show");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
 
-body {
-  font-size: 15pt;
-  background: #eeeeee;
-
-}
-
-h1 {
-  color: #3FABFF;
-  padding-top: 50px;
-  padding-bottom: 30px;
-  text-align: center;
-  font-weight: bold;
-}
-
-li {
-  font-weight: bold;
-  text-align: center;
-  padding: 20px;
-  color: peru;
-}
-
-label {
-  position: absolute;
-  background: aliceblue;
-  width: 91.8%;
-  padding: 7px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  margin-top: -15px;
-}
-
-input {
-  padding: 10px;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  border: 2px dashed #ccc;
-  width: 100%;
-  background: azure;
-}
-
-span {
-  position: relative;
-  text-align: left;
-}
-
-input[type=submit],
-input[type=button] {
-  background: dodgerblue;
-  color: white;
-  border-radius: 10px;
-  border-style: none;
-}
-
-input[type=submit]:hover {
-  background: seagreen;
-}
-
-.row {
-  padding-bottom: 50px;
-}
-
-div[class=text2] h1 {
-  color: seagreen;
-}
-
+ var attempt = 3;
+  function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == "prashantkumarjoyal@gmail.com" && password == "12345") {
+  alert("Welcome to The PKJoyal.");
+  window.location = "home.html";
+  return true;
+  }
+  else {
+  attempt--;
+  alert("You have left " + attempt + " attempt;");
+  if (attempt == 0) {
+  document.getElementById("username").disabled = true;
+  document.getElementById("password").disabled = true;
+  document.getElementById("submit").disabled = true;
+  return false;
+  }
+  }
+  }
